afa5ebd92afb5199d759a3f6f3f504f6d09902ec
drhodesws <drhodesws@gmail.com>
2025-11-20 18:38:16 -0600

    Delete routes directory

--- routes/contacts.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from routes.auth import login_required  # Import decorator from auth
-
-contacts_bp = Blueprint('contacts', __name__, url_prefix='/contacts')
-
-@contacts_bp.route('/contacts', methods=['GET'])
-@login_required
-def contacts_page():
-    return render_template('contacts.html')
