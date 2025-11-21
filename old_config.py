1f789671c882928a1f8e344cb5dfea6f1e5ad07e
drhodesws <drhodesws@gmail.com>
2025-11-20 18:40:09 -0600

    Delete old_config.py

--- old_config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key_here')  # Use environment variable or default
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///maritime.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
